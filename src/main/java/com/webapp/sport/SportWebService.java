dbdab5814e68af8684c66946829355af66d02bd2
Zamfir Arina <zamfirarina@gmail.com>
2019-11-21 10:56:10 +0200

    first commit

--- src/main/java/com/webapp/sport/SportWebService.java
+++ src/main/java/com/webapp/sport/SportWebService.java
@@ -1,26 +1,45 @@
 package com.webapp.sport;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("sport")
+@RequestMapping("/sport")
 public class SportWebService {
+    TrainersRepository tRep = new TrainersRepository();
+    PlayersRepository pRep = new PlayersRepository();
 
     @GetMapping
     public String test() {
         return "First try in Spring Web";
     }
+
     @GetMapping("/trainers")
-    public List<String> studentsList(){
-        return List.of("Andrei","Alexandrina","Dumitru","Andrei","Zina");
-    }
-    @GetMapping("/players")
-    public List<String> teachersList(){
-        return List.of("Vadim","Anatolie","Silvia","Ana","Petrea");
+    public List<String> trainersList() {
+        return tRep.getTrainers();
     }
 
+    @GetMapping("/players")
+    public List<String> playersList() {
+        return pRep.getPlayers();
+    }
+
+    @PostMapping("addTrainers/{name}")
+    public void addTrainers(@PathVariable String name) {
+       tRep.addTrainers (name);
+
+    }
+
+    @PutMapping ("/addPlayers/{name}")
+    public void addPlayers(@PathVariable("name")String name) {
+        pRep.addPlayers(name);
+
+    }
+
+    @DeleteMapping("delPlayers/{name}")
+    public void delPlayers(@PathVariable("name") String name) {
+        pRep.delPlayers(name);
+    }
 }
